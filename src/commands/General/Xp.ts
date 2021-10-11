d8dc57e02f349cb210bd6a14576d4498ffcb47a6
well300 <90952986+well300@users.noreply.github.com>
2021-10-11 16:35:11 +0530

    Update Xp.ts

--- src/commands/General/Xp.ts
+++ src/commands/General/Xp.ts
@@ -15,6 +15,11 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
+        run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
+            const n = [
+            './assets/videos/Sukuna/sukuna-xp.mp4'
+        ]
+        let sukuna = n[Math.floor(Math.random() * n.length)]
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
         let username = user === M.sender.jid ? M.sender.username : 'Person'
@@ -23,6 +28,8 @@ export default class Command extends BaseCommand {
             // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
             username = user.split('@')[0]
         }
-        return void (await M.reply(`${username}'s XP: ${(await this.client.getUser(user)).Xp || 0}`))
+            return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `${username}'s XP: ${(await this.client.getUser(user)).Xp || 0} \n`
     }
 }
