5b1f79a65577ffc7530ebde5deed065ff252731a
well300 <90952986+well300@users.noreply.github.com>
2021-10-14 12:56:43 +0530

    Update Features.ts

--- src/commands/Misc/Features.ts
+++ src/commands/Misc/Features.ts
@@ -23,8 +23,8 @@ export default class Command extends BaseCommand {
         return void this.client.sendMessage(M.from, { url: shinobu }, MessageType.video, {
             mimetype: Mimetype.gif,
             caption: `🦋 ꜱʜɪɴᴏʙᴜ\n\n⚜ *Features* : 
-|Media| usage | 
-|----|--------| 
+|Media| usage |
+
 |Lyrics| Gives you the lyrics of the given song|
 |Play| Plays the given title|
 |Spotify|  Downloads songs from Spotify| 
@@ -33,7 +33,7 @@ export default class Command extends BaseCommand {
 |YouTube search| Searches the given title from YouTube| 
 
 |General| usage |
-|----|--------|
+
 |Admins| Tags all the admins in a given group|
 |Help| Displays all the available commands of bot in the group|
 |Mods| Displays the moderator's contact info|
@@ -41,18 +41,18 @@ export default class Command extends BaseCommand {
 |Xp| Views the tagged user's xp|
 
 |Fun| usage|
-|----|--------|
+
 |Triggered| Tag a photo while commanding, given image would be triggered xD|
 |Chess| Launches a game of chess straight in the group chat, YES THIS IS THE FUTURE! EMBRACE IT!|
 
 |Misc| usage|
-|----|--------|
+
 |Hi| WELL|
-|Chitoge| Views info :v|
+|Shinobu| Views info :v|
 |Retrieve| Retrieve viewOnceMessage WhatsApp Message|
 
 |Moderation| usage|
-|----|--------|
+
 |Activate| Activates certain bot features in a group chat|
 |Deactivate| Deactivates certain bot features in a group chat|
 |Delete| Deletes the quoted message, admin only feature|
@@ -61,21 +61,15 @@ export default class Command extends BaseCommand {
 |Demote| Demotes the tagged user as a standard participant from an admin if he's one. Can work only when commanding user is himself a group admin|
 |Remove| Removes the tagged user from the group, can only work if the bot is an admin and the one commanding this tool|
 
-|Nsfw| usage|
-|----|--------|
-|Nsfw-neko| Sends you random nsfw neko image|
-|Nsfw-waifu| Sends you random nsfw waifu image|
-|Trapped-waifu| Sends you random nsfw trapped waifu image|
-
 |Utils| usage|
-|----|--------|
+
 |Blur| Blurs the tagged image or the pfp of the user's commanding it|
 |Sticker| Converts the given gif/image into a webp sticker|
 |Subred| A random subreddit post based on the specified search would be fetched. (Sometimes nsfw posts can too be fetched so be careful)|
 |Steal| Steals the given sticker (This command is under development)|
 
 |Weeb| usage|
-|----|--------|
+
 |Kitsune| Sends you random kitsune image|
 |Loli| Sends you random anime loli image|
 |Megumin-paper| Sends you random Megumin (From Konosuba) image|
@@ -85,13 +79,16 @@ export default class Command extends BaseCommand {
 |Waifu| Sends you random anime waifu image|
 
 |Dev| usage|
-|----|--------|
-|Disable| Disables the specified bot command from being used in a group chat|
-|Enable| Enables the specified bot command so that it can be used again in a group chat|
-|Eval| Evaluates JavaScript|
-|Google| Searches the given title in google (GOOGLE_API_KEY required to perform this activity)| 
-|Ban| Bans the given user from using the bot|
-|Unban| Unbans the given user, after this he/she can use the bot again|\n\n` }
+
+Disable: Disables the specified bot command from being used in a group chat.
+
+Enable: Enables the specified bot command so that it can be used again in a group chat.
+
+Google: Searches the given title in google.
+
+Ban: Bans the given user from using the bot.
+
+Unban: Unbans the given user, after this he/she can use the bot again. \n\n` }
         )
     }
 }
