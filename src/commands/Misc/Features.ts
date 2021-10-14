07877d2ef1664e3f8a7d8b7144a51c058876fc72
well300 <90952986+well300@users.noreply.github.com>
2021-10-14 13:45:41 +0530

    Update Features.ts

--- src/commands/Misc/Features.ts
+++ src/commands/Misc/Features.ts
@@ -29,44 +29,34 @@ Lyrics: Gives you the lyrics of the given song.
 Play: Plays the given title.
 Spotify:  Downloads songs from Spotify.
 YouTube video: Downloads video from YouTube.
-YouTube audio| Downloads audio from YouTube.
-YouTube search| Searches the given title from YouTube. 
+YouTube audio: Downloads audio from YouTube.
+YouTube search: Searches the given title from YouTube. 
 
-|General| usage |
+*General*
 
-|Admins| Tags all the admins in a given group|
-|Help| Displays all the available commands of bot in the group|
-|Mods| Displays the moderator's contact info|
-|Profile| Displays a generated profile of the tagged/mentioned user|
-|Xp| Views the tagged user's xp|
+Admins: Tags all the admins in a given group.
+Help: Displays all the available commands of bot in the group.
+Mods: Displays the moderator's contact info.
+Profile: Displays a generated profile of the tagged/mentioned user.
+Xp: Views the tagged user's xp.
 
-|Fun| usage|
+*Fun*
 
-|Triggered| Tag a photo while commanding, given image would be triggered xD|
-|Chess| Launches a game of chess straight in the group chat, YES THIS IS THE FUTURE! EMBRACE IT!|
+Triggered: Tag a photo while commanding, given image would be triggered xD.
+Chess: Launches a game of chess straight in the group chat, YES THIS IS THE FUTURE! EMBRACE IT!
 
-|Misc| usage|
+*Misc*
 
-|Hi| WELL|
-|Shinobu| Views info :v|
-|Retrieve| Retrieve viewOnceMessage WhatsApp Message|
-
-|Moderation| usage|
-
-|Activate| Activates certain bot features in a group chat|
-|Deactivate| Deactivates certain bot features in a group chat|
-|Delete| Deletes the quoted message, admin only feature|
-|Everyone| Tags all participants of the given group|
-|Promote| Promotes the tagged user as a admin on group chat, can be executed only if the one commanding is group admin|
-|Demote| Demotes the tagged user as a standard participant from an admin if he's one. Can work only when commanding user is himself a group admin|
-|Remove| Removes the tagged user from the group, can only work if the bot is an admin and the one commanding this tool|
+Hi: WELL.
+Shinobu: Views info :v
+Retrieve: Retrieve viewOnceMessage WhatsApp Message.
 
 *Utils*
 
-Blur| Blurs the tagged image or the pfp of the user's commanding it|
-Sticker| Converts the given gif/image into a webp sticker|
-Subred| A random subreddit post based on the specified search would be fetched. (Sometimes nsfw posts can too be fetched so be careful)|
-Steal| Steals the given sticker (This command is under development)|
+Blur: Blurs the tagged image or the pfp of the user's commanding it.
+Sticker: Converts the given gif/image into a webp sticker.
+Subred: A random subreddit post based on the specified search would be fetched. (Sometimes nsfw posts can too be fetched so be careful)
+Steal; Steals the given sticker (This command is under development)
 
 *Weeb*
 
