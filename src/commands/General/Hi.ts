a5260aa120e89e2b5e69b0a6283bfb2695f16666
well300 <90952986+well300@users.noreply.github.com>
2021-10-15 23:57:33 +0530

    Add files via upload

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -1,3 +1,5 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
@@ -7,14 +9,39 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'hi',
-            description: 'Generally used to check if bot is Up',
-            category: 'general',
-            usage: `${client.config.prefix}hi`,
-            baseXp: 0
+            description: 'Say hi to the bot.',
+            category: 'misc',
+            usage: `${client.config.prefix}hi`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void (await M.reply(`🦋 Hello ${M.sender.username}!`))
+        const n = [
+            './assets/videos/Shinobu/shinobu-hi.mp4'
+        ]
+        let shinobu = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: shinobu }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `𝑀𝑜𝓈𝒽𝒾 𝓂𝑜𝓈𝒽𝒾, 𝒟𝒶𝒾𝒿𝑜𝓊𝒷𝓊 𝒹𝑒𝓈𝓊𝓀𝒶? \n` }
+        )
     }
 }
+          
+       
+
+
+    
+        
+           
+           
+            
+            
+        
+    
+
+    
+        
+           
+           
+           
+   
