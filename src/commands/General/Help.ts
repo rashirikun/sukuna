7f75f2a094b4702c684202137ce6895c3555f4ca
well300 <90952986+well300@users.noreply.github.com>
2021-10-16 00:14:47 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -2,9 +2,6 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
-import { MessageType, Mimetype } from '@adiwajshing/baileys'
-import request from '../../lib/request'
-
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
@@ -19,49 +16,29 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
-            const n = [
-            './assets/videos/Sinobu/shinobu.mp4'
-        ]
-        let shinobu = n[Math.floor(Math.random() * n.length)]
         if (!parsedArgs.joined) {
             const commands = this.handler.commands.keys()
             const categories: { [key: string]: ICommand[] } = {}
             for (const command of commands) {
                 const info = this.handler.commands.get(command)
                 if (!command) continue
-                if (!info?.config?.category || info.config.category === 'dev') continue
+                if (!info?.config?.category) continue
                 if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
                 else {
                     categories[info.config.category] = []
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `
-╭─「（＾∀＾●）ﾉｼ」
-│⋊ ᴜꜱᴇʀ: *${M.sender.username}*
-│⋊ ɴᴀᴍᴇ: ꜱʜɪɴᴏʙᴜ
-│⋊ ᴘʀᴇꜰɪx: !
-│⋊ ᴏᴡɴᴇʀ: <!mod>
-╰────────────┈平和                 \n\n`
-            const keys = Object.keys(categories)
+            let text = `🦋 *shinobu's Command List* 🦋\n\n`
+            const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
             for (const key of keys)
-                text += `❈┈ᕮ✧${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}✧ᕭ┈❈\n⪩ \`\`\`${categories[
+                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
                     key
                 ]
                     .map((command) => command.config?.command)
-                    .join('\n')}\`\`\`\n\n`
-            return void this.client.sendMessage(M.from, { url: shinobu }, MessageType.video, {
-            mimetype: Mimetype.gif,
-            caption: `${text} 
- ──❅┈[ ꜱʜɪɴᴏʙᴜ ʙᴏᴛ ]┈❅───
-
-┌────────────┈❅
-│   〽 ꜱʜɪɴᴏʙᴜ
-│   ©️ Synthesized Infinity Botto
-└────────────┈⁂
-❅┈[𝐇𝐚𝐯𝐞 𝐆𝐫𝐞𝐚𝐭 𝐃𝐚𝐲]┈❅
-
-🎗 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
+                    .join(', ')}\`\`\`\n\n`
+            return void M.reply(
+                `${text} 🗃️ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
             )
         }
         const key = parsedArgs.joined.toLowerCase()
@@ -69,15 +46,15 @@ export default class Command extends BaseCommand {
         if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
         const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
         M.reply(
-            `🚀 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
+            `🎫 *Command:* ${this.client.util.capitalize(command.config?.command)}\n🎗️ *Status:* ${
                 state ? 'Disabled' : 'Available'
-            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
-                command.config.aliases
-                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
+            }\n🀄 *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
+                command.config.aliases && command.config.command !== 'react'
+                    ? `\n🍥 *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
                     : ''
-            }\n🎐 *Group Only:* ${this.client.util.capitalize(
+            }\n🃏 *Group Only:* ${this.client.util.capitalize(
                 JSON.stringify(!command.config.dm ?? true)
-            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
+            )}\n🎀 *Usage:* ${command.config?.usage || ''}\n\n🔖 *Description:* ${command.config?.description || ''}`
         )
     }
 
