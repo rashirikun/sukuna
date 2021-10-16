3f48d9e8d1a6f15dc57f63e67e5d3c0e64b6b3b0
well300 <90952986+well300@users.noreply.github.com>
2021-10-16 18:05:58 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -44,7 +44,7 @@ export default class Command extends BaseCommand {
 ╰────────────┈平和                 \n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
-                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}* ${this.emojis[keys.indexOf(key)]}\n‎ \`\`\`${categories[
+                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}* ${this.emojis[keys.indexOf(key)]}\n\n• \`\`\`${categories[
                     key
                 ]
                     .map((command) => command.config?.command)
