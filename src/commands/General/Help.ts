b28fdab10e00ec52941921876698d4eabae1a6cf
well300 <90952986+well300@users.noreply.github.com>
2021-10-16 16:14:15 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -1,9 +1,9 @@
-import { MessageType, Mimetype } from '@adiwajshing/baileys'
-import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import request from '../../lib/request'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
