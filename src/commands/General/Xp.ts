0c8b7cd51f876f60265e9e5aec20c78f5ee2a225
well300 <90952986+well300@users.noreply.github.com>
2021-10-16 14:30:37 +0530

    Update Xp.ts

--- src/commands/General/Xp.ts
+++ src/commands/General/Xp.ts
@@ -41,8 +41,7 @@ export default class Command extends BaseCommand {
             MessageType.image,
             undefined,
             undefined,
-            `🧧 *${username}'s* \n
-             🌟 XP: ${data.Xp || 0}`
+            `🧧 *${username}'s* \n🌟 XP: ${data.Xp || 0}`
         )
     }
 }
